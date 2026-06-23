0f584a959fc7a4ccba847f11f3964a54eeb64504
Sivaarthi <sivaarthigunsekaran@gmail.com>
2026-06-23 09:23:02 +0530

    Initial commit

---
+++ even.java
@@ -0,0 +1,10 @@
+class even{
+    public static void main(String []args){
+        int a=901;
+        //return the last digit of a number
+        int dig=a%10;
+        System.out.println(dig);
+         }
+}
+        
+    
